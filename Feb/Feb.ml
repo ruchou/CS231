fb826367163157b173e474c552930f9b1eac01c2
yanrujhou <joe04024@gmail.com>
2020-02-10 15:50:48 -0800

    2/10

--- Feb/Feb.ml
+++ Feb/Feb.ml
@@ -3,3 +3,23 @@
 let swap f = (fst f, snd f);;
 
 let a = fst (5+5,  "hi");;
+
+
+
+type intorbool = Left of int | Right of bool;;
+
+Left 34;
+Right true;
+
+let toInt iorb =
+    match iorb with
+        Left i -> i
+        |Right b -> if b then 1 else 0;;
+
+
+type ('a,'b) aorb = Left of 'a | Right of 'b;;
+
+let f pair =
+    match snd pair with
+        Left x1 -> (fst pair) x1
+        |Right x2 -> x2;;
